e9c35f3c04036d4a137025e97f209b7c57dee6bc
114929 <u84878949@cbi-bs1.server.lan>
2017-12-12 11:35:41 -0500

    Initial Commit

---
+++ donaciones/wp-config.php
@@ -0,0 +1,82 @@
+<?php
+/** 
+ * Configuración básica de WordPress.
+ *
+ * Este archivo contiene las siguientes configuraciones: ajustes de MySQL, prefijo de tablas,
+ * claves secretas, idioma de WordPress y ABSPATH. Para obtener más información,
+ * visita la página del Codex{@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} . Los ajustes de MySQL te los proporcionará tu proveedor de alojamiento web.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
+/** El nombre de tu base de datos de WordPress */
+define('DB_NAME', 'db707816683');
+
+/** Tu nombre de usuario de MySQL */
+define('DB_USER', 'dbo707816683');
+
+/** Tu contraseña de MySQL */
+define('DB_PASSWORD', 'Z9%Mdej[ZRiM');
+
+/** Host de MySQL (es muy probable que no necesites cambiarlo) */
+define('DB_HOST', 'db707816683.db.1and1.com');
+
+/** Codificación de caracteres para la base de datos. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** Cotejamiento de la base de datos. No lo modifiques si tienes dudas. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Claves únicas de autentificación.
+ *
+ * Define cada clave secreta con una frase aleatoria distinta.
+ * Puedes generarlas usando el {@link https://api.wordpress.org/secret-key/1.1/salt/ servicio de claves secretas de WordPress}
+ * Puedes cambiar las claves en cualquier momento para invalidar todas las cookies existentes. Esto forzará a todos los usuarios a volver a hacer login.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', '3rWNkh3}>gl<QJ)<P>[V,C[c]UNfEiJd&n2mj[j^Pcp3]^bH?98 tP8|_;_w:{0G');
+define('SECURE_AUTH_KEY', 'Vq1ab*>jyY;KY+jXH#{$YrYoETU`7`a{>iO^+mI1y:{ht9UCg5C%|84ymCL4%PZv');
+define('LOGGED_IN_KEY', 'YC{w/twqfg|.V`rdcaINxKoGeU=jKwWvTPb0Lx,%6z#SeM^Mzav1_bPBc33DCxTv');
+define('NONCE_KEY', 'yk{~?*+qCICKz`&bk0dQg1xeQ|H?42XdBjDcNhRsLm.DF@P:a[QZ.{Jf:wFv1,.T');
+define('AUTH_SALT', '<1wiO&Sk<@6Kx(s Eo8]BK0Sx*69p}LR)8/DRl}pk5v8xKGd$zKELK$J__RG3zc6');
+define('SECURE_AUTH_SALT', '-8H{]3fCG(QueAf{/i>3.YsnJr%]c)j9JNsa3%I_]Tf*(1qR`[LcDC77+84@u}rR');
+define('LOGGED_IN_SALT', 'J!;u73!oTcFv06>1|UXC%qHvl%CvkS]N$/YpeA#So~[!2(3l&o@$*`BIf]&s3S[E');
+define('NONCE_SALT', 'e>Wk*HQ+uEb,oP4~<E[R>><Pvgv1/7M8gb3;)tSx4^gL%/7dK :9Al&:S2du-&%a');
+
+/**#@-*/
+
+/**
+ * Prefijo de la base de datos de WordPress.
+ *
+ * Cambia el prefijo si deseas instalar multiples blogs en una sola base de datos.
+ * Emplea solo números, letras y guión bajo.
+ */
+$table_prefix  = 'wp_';
+
+
+/**
+ * Para desarrolladores: modo debug de WordPress.
+ *
+ * Cambia esto a true para activar la muestra de avisos durante el desarrollo.
+ * Se recomienda encarecidamente a los desarrolladores de temas y plugins que usen WP_DEBUG
+ * en sus entornos de desarrollo.
+ */
+define('WP_DEBUG', false);
+
+/* ¡Eso es todo, deja de editar! Feliz blogging */
+
+/** WordPress absolute path to the Wordpress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
---
+++ donaciones/wp-content/plugins/vc-templates-import-export/readme.txt
@@ -0,0 +1,69 @@
+=== VC Templates Import & Export ===
+Contributors: Tomiup
+Tags: export visual composer template, import visual composer template, visual composer, vc addon, visual composer addon, vc, templates, export, import, wpbakery page builder, wpbakery
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=GHW9BN3JUJ98C
+Requires at least: 4.8.2
+Tested up to: 4.8.2
+Requires PHP: 5.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/license-list.html#GPLCompatibleLicenses
+
+Export & Import Visual Composer Templates (Saved Templates/My Templates) in few clicks.
+
+== Description ==
+Export & Import <strong>Visual Composer</strong> Templates (Saved Templates/My Templates) in few clicks.
+
+
+This plugin is addon for <a href="http://bit.ly/vc-plugin" target="_blank"><strong>WPBakery Page Builder</strong></a> (formerly Visual Composer).
+
+
+== Features ==
+* <strong>Import/Export templates</strong> in JSON format – take your custom templates with you
+* <strong>Single Template Export</strong> – export only those templates you wish
+
+== Compatible With ==
+* WPBakery Page Builder (formerly <a href="http://bit.ly/vc-plugin" target="_blank"><strong>Visual Composer</strong></a>) 4.12.x/ 5.1.x and 5.4 (Latest version)
+
+== Feedback ==
+If you like this plugin, then please consider leaving us a good rating and a review feedback</a>.
+If would like, you can follow us on <a rel="nofollow" href="https://www.facebook.com/tomiupdotcom">Facebook</a>
+
+
+== Installation ==
+1. Upload the plugin files to the "/wp-content/plugins/vc-templates-import-export/" directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the "Plugins" screen in WordPress.
+3. Visual Composer -> Templates Import & Export screen to use the plugin.
+
+== Screenshots ==
+1. screenshot-1.png
+2. screenshot-2.png
+3. screenshot-3.png
+4. screenshot-4.png
+
+== Changelog ==
+= 0.1.3 =
+* Improvements function export template.
+
+= 0.1.2 =
+* Compatible with Visual Composer 5.4
+
+= 0.1.1 =
+* Added: Export 1 or multi templates.
+* Added: Import 1 or multi templates.
+
+= 0.1.0 =
+* Initial Release.
+
+== Upgrade Notice ==
+= 0.1.3 =
+* Improvements function export template.
+
+= 0.1.2 =
+* Compatible with Visual Composer 5.4
+
+= 0.1.1 =
+* Improve Export & Import templates.
+
+= 0.1.0 =
+* Initial Release.
